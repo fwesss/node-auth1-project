5c69c48ba185d20d68e6f03962f69baff20dff09
Westley Feller <feller.westley@gmail.com>
2020-02-12 18:49:37 -0800

    Routing for /api

--- api/server.ts
+++ api/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-import configureMiddleware from './middleware'
+import configureMiddleware from './api.middleware'
+import apiRouter from './api.router'
 
 const server = express()
 
 configureMiddleware(server)
+
+server.use('/api', apiRouter)
 
 export default server
