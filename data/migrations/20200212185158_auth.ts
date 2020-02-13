628173e420d83ebff2d0da7ea3a5de261d463449
Westley Feller <feller.westley@gmail.com>
2020-02-12 18:59:02 -0800

    Migration: create users table

---
+++ data/migrations/20200212185158_auth.ts
@@ -0,0 +1,14 @@
+import Knex, { SchemaBuilder } from 'knex'
+
+export const up = (knex: Knex): SchemaBuilder =>
+  knex.schema.createTable('users', table => {
+    table.increments()
+    table
+      .string('username')
+      .notNullable()
+      .unique()
+    table.string('password').notNullable()
+  })
+
+export const down = (knex: Knex): SchemaBuilder =>
+  knex.schema.dropTableIfExists('users')
