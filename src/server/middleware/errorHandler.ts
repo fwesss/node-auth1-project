eabe441447ec04842f849283dd51c6a85c4cf2bc
Westley Feller <feller.westley@gmail.com>
2020-02-15 18:57:23 -0800

    Using guaranteedPromise in place of try/catch
    Doesn't work with QueryBuilder because it's not an actual promise. It just has Promise-like methods.

--- src/server/middleware/errorHandler.ts
+++ src/server/middleware/errorHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
 import { Request, Response, NextFunction } from 'express'
 import { ValidationError } from '../../utils/validator'
@@ -7,6 +8,25 @@ export class UnauthorizedError extends Error {
     super()
     this.name = 'UnauthorizedError'
     this.message = error.message
+    Error.call(this, error.message)
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export class DatabaseError extends Error {
+  dbMessage: {
+    errno: number
+    code: string
+  }
+
+  constructor(error: {
+    message: string
+    dbMessage: { errno: number; code: string }
+  }) {
+    super()
+    this.name = 'DatabaseError'
+    this.message = error.message
+    this.dbMessage = error.dbMessage
     Error.call(this, error.message)
     Error.captureStackTrace(this, this.constructor)
   }
@@ -28,12 +48,20 @@ const errorHandler = (
       .json({ message: error.message, errors: error.invalidations })
   } else if (error instanceof UnauthorizedError) {
     res.status(401).json({ message: error.message })
+  } else if (error instanceof DatabaseError) {
+    res.status(500).json({
+      name: error.name,
+      message: error.message,
+      dbMessage: error.dbMessage,
+    })
   } else {
     console.error({
+      name: error.name,
       message: error.message,
       stack: error.stack,
     })
     res.status(500).json({
+      name: error.name,
       message: error.message,
       ...(process.env.NODE_ENV !== 'production' && { stack: error.stack }),
     })
